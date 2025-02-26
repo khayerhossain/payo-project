4a0f842f1f25a9d31ce39a2501d6465fa84be960
khayerhossain <khayerhossain62@gmail.com>
2025-02-26 18:38:27 +0800

    login section complete

---
+++ scripts/payo.js
@@ -0,0 +1,19 @@
+document.getElementById('login-btn').addEventListener('click',
+    function (event) {
+        event.preventDefault();
+        const accountNumber = document.getElementById('number-value').value
+        const pin = document.getElementById('pin-value').value
+        const pinConvert = parseInt(pin);
+        if (accountNumber.length === 11) {
+            if (pinConvert === 1234) {
+                window.location.href = "./main.html"
+            }
+            else {
+                alert("wrong PIN")
+            }
+        }
+        else {
+            alert("Need a valid account number")
+        }
+
+    })
